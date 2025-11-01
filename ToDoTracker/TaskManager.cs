2f295d1ea739420e1a5e55e48c1413207eaaa147
MUBINSKT <mubinskt@gmail.com>
2025-11-01 12:57:20 +0530

    Add project files.

---
+++ ToDoTracker/TaskManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoTracker
+{
+    class TaskManager
+    {
+        private List<ToDoTask> toDoTasks = new List<ToDoTask>();
+
+        public List<ToDoTask> ToDoTasks
+        {
+            get { return toDoTasks; }
+            set { toDoTasks = value; }
+        }
+
+        public bool AddTask()
+        {
+
+            return false;
+        }
+
+        public bool DeleteTask()
+        {
+
+            return false;
+        }
+    }
+}
---
+++ ToDoTracker/ToDoTask.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoTracker
+{
+    enum TaskPriority
+    {
+        High,
+        Medium,
+        Low
+    }
+
+    enum TaskState
+    {
+        Created,
+        Pending,
+        Completed
+    }
+
+    class ToDoTask
+    {
+        string Task { get; set; }
+
+        string Description { get; set; }
+
+        TaskPriority TaskPriority { get; set; } = TaskPriority.Medium;
+
+        DateTime StartDateTime { get; set; }
+
+        DateTime EndDateTime { get; set; }
+
+        TaskState taskState { get; set; } = TaskState.Created;
+
+    }
+}
